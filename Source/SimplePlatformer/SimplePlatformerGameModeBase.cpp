60764dd79c0e07d1445b20dd7aaf396f5495ac9d
Michael Geffre <geffrejunior92@gmail.com>
2020-04-19 15:21:13 -0700

    Changing GameMode to Blueprint

--- Source/SimplePlatformer/SimplePlatformerGameModeBase.cpp
+++ Source/SimplePlatformer/SimplePlatformerGameModeBase.cpp
@@ -7,10 +7,5 @@
 
 ASimplePlatformerGameModeBase::ASimplePlatformerGameModeBase()
 {
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/PlayerCharacter_BP"));
-	// static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/DIY/MyDIY_PlayerCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	
 }
